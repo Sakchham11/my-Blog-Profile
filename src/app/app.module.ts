187f206a17a334b81db667ea920ecfa121c3fbf2
sakchham <sakchham1110@gmail.com>
2023-08-17 23:03:51 +0530

    main page footer profile/intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
